842dcabbd26236ccb73bb8fda91579182c1d5f86
clem <mail@clemens-cords.com>
2023-06-21 13:57:26 +0200

    added dummy

---
+++ dummy.hpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) Clemens Cords (mail@clemens-cords.com), created 6/21/23
+//
+
+#pragma once
+
+#include <iostream>
+
+static void test()
+{
+    std::cout << "test" << std::endl;
+}
