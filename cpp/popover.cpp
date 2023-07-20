560c862a15fd9327b37fabde451b6a127598a5ab
clem <mail@clemens-cords.com>
2023-07-21 00:09:09 +0200

    fixed popover attachment

--- cpp/popover.cpp
+++ cpp/popover.cpp
@@ -7,14 +7,10 @@ void implement_popover(jlcxx::Module& module)
     auto popover = module.add_type(Popover)
     .add_type_method(Popover, popup, !)
     .add_type_method(Popover, popdown, !)
-    .add_type_method(Popover, present, !)
     .method("set_child!", [](Popover& popover, void* widget){
         popover.set_child(*((Widget*) widget));
     })
     .add_type_method(Popover, remove_child, !)
-    .method("attach_to!", [](Popover& popover, void* widget){
-        popover.attach_to(*((Widget*) widget));
-    })
     .add_type_method(Popover, set_relative_position, !)
     .add_type_method(Popover, get_relative_position)
     .add_type_method(Popover, set_autohide, !)
