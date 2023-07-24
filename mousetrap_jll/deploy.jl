dbea4a185f31b78ad249ad03b5d9275476047a9a
clem <mail@clemens-cords.com>
2023-07-24 19:33:06 +0200

    updated jll

--- mousetrap_jll/deploy.jl
+++ mousetrap_jll/deploy.jl
@@ -2,38 +2,17 @@
 # julia deploy.jl [--deploy=$platforms] [--build=$platforms]
 #
 
+const VERSION = "0.1.1"
 const mousetrap_commit = "df09eb3b41eeb70ec67c969674ed775d2c0239cd"
-const mousetrap_julia_binding_commit = "2f3e55b95fd29d1589c39a2c407edc1ea5d2d556"
+const mousetrap_julia_binding_commit = "0fdc8c77bacd842a3f75a14b78e671e0a9515978"
 
-build_linux = true
 deploy_linux = true
-build_windows = true
 deploy_windows = true
 
-#=
-for arg in ARGS
-    if occursin("build", arg)
-        if occursin("windows", arg) || occursin("Windows", arg) || occursin("all", arg)
-            global build_windows = true
-        elseif occursin("linux", arg) || occursin("Linux", arg) || occursin("all", arg)
-            global build_linux = true
-        end
-    end
+const deploy_local = false
+# if local, files will be written to ~/.julia/dev/mousetrap_[linux,windows]_jll 
 
-    if occursin("deploy", arg)
-        if occursin("windows", arg) || occursin("Windows", arg) || occursin("all", arg)
-            global deploy_windows = true
-        elseif occursin("linux", arg) || occursn("Linux", arg) || occursin("all", arg)
-            global deploy_linux = true
-        end
-    end
-end
-=#
-
-println("build linux    : $build_linux")
 println("deploy linux   : $deploy_linux")
-println()
-println("build windows  : $build_windows")
 println("deploy windows : $deploy_windows")
 
 ## Configure
@@ -45,7 +24,8 @@ function configure_file(path_in::String, path_out::String)
     for line in eachline(file_in)
         write(file_out, replace(line,
             "@MOUSETRAP_COMMIT@" => mousetrap_commit,
-            "@MOUSETRAP_JULIA_BINDING_COMMIT@" => mousetrap_julia_binding_commit
+            "@MOUSETRAP_JULIA_BINDING_COMMIT@" => mousetrap_julia_binding_commit,
+            "@VERSION@" => VERSION
         ) * "\n")
     end
     
@@ -74,7 +54,11 @@ end
 
 if deploy_linux
     @info "Deploying `mousetrap_linux_jll`"
-    run(`julia build_tarballs.jl --deploy=Clemapfel/mousetrap_linux_jll`)
+    if deploy_local
+        run(`julia build_tarballs.jl --deploy=local`)
+    else
+        run(`julia build_tarballs.jl --deploy=Clemapfel/mousetrap_linux_jll`)
+    end
 end
 
 cd("..")
@@ -87,7 +71,11 @@ end
 
 if deploy_windows
     @info "Deploying `mousetrap_windows_jll`"
-    run(` julia build_tarballs.jl --deploy=Clemapfel/mousetrap_windows_jll`)
+    if deploy_local
+        run(`julia build_tarballs.jl --deploy=local`)
+    else
+        run(`julia build_tarballs.jl --deploy=Clemapfel/mousetrap_windows_jll`)
+    end
 end
 
 cd("..")
